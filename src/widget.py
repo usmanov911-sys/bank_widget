8ec80a1f8bbb787209821e41f76eba4f7f38fa7b
Arthur <Arthur@list.ru>
2026-07-14 15:46:10 +0300

    add modifying the widget.py file

--- src/widget.py
+++ src/widget.py
@@ -9,7 +9,7 @@ def mask_account_card(info: str) -> str:
     label = " ".join(parts[:-1])
     number = parts[-1]
 
-    if len(number) >= 16:
+    if len(number) >= 20:
         masked = get_mask_card_number(number)
     else:
         masked = get_mask_account(number)

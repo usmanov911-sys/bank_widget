8bf3e4bb9a7e8975c9633b55d7471484f407f5a5
Arthur <Arthur@list.ru>
2026-08-05 03:09:25 +0300

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,30 @@
+
+import json
+from typing import List, Dict
+
+
+def load_json_file(file_path: str) -> List[Dict]:
+    """
+    Загружает данные из JSON-файла и возвращает их как список словарей.
+
+    Args:
+        file_path (str): Путь к файлу JSON.
+
+    Returns:
+        List[Dict]: Список транзакций или пустой список,
+                   если файл не найден, пуст или содержит неверные данные.
+    """
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+            if isinstance(data, list):
+                return data
+            else:
+                return []
+
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
+
